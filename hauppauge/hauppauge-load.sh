a7bea154697b413e5185356ea117baee6b2f5ffd
Vincent Fortier <th0ma7@gmail.com>
2019-05-14 19:56:38 -0400

    Updated module load & unload scripts

--- hauppauge/hauppauge-load.sh
+++ hauppauge/hauppauge-load.sh
@@ -8,9 +8,9 @@ sudo insmod /lib/modules/videodev.ko
 sudo insmod /lib/modules/v4l2-common.ko
 sudo insmod /lib/modules/tveeprom.ko
 
-# Load Hauppauge updated drivers
+# Load Hauppauge necessary tuners, dvb-frontend and updated drivers
+sudo insmod /lib/modules/si2157.ko
+sudo insmod /usr/local/lib/modules/$(uname -r)/media.ko
+sudo insmod /usr/local/lib/modules/$(uname -r)/lgdt3306a.ko
 sudo insmod /usr/local/lib/modules/$(uname -r)/em28xx.ko
 sudo insmod /usr/local/lib/modules/$(uname -r)/em28xx-dvb.ko
-sudo insmod /usr/local/lib/modules/$(uname -r)/lgdt3306a.ko
-sudo insmod /usr/local/lib/modules/$(uname -r)/media.ko
-sudo insmod /lib/modules/si2157.ko
---
+++ hauppauge/hauppauge-unload.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+SERVICE="pkgctl-tvheadend"
+MODULES="em28xx-dvb em28xx lgdt3306a media si2157 tveeprom v4l2-common videodev dvb-usb rc-core dvb-core"
+
+GetServiceStatus() {
+   case $(sudo synoservice --status $SERVICE | grep "\[$SERVICE\].*start") in
+      "\[$SERVICE\].*start" ) return "start";;
+       "\[$SERVICE\].*stop" ) return "stop";;
+	                      * ) return "unknown";;
+   esac
+}
+
+#echo -ne "Stopping $SERVICE... "
+printf '%-33s' "Stopping $SERVICE..."
+[ "$GetServiceStatus" = "start" ] \
+   && sudo synoservice --pause $SERVICE \
+   && echo "OK" \
+   || echo "N/A"
+
+# Unload Hauppauge updated drivers
+echo "Unloading kernel modules... "
+for module in $MODULES
+do
+   printf '\t%-25s' $module
+
+   status=$(lsmod | grep "^$module ")
+   if [ $? -eq 0 -a "status" ]; then
+      sudo rmmod $module
+	  echo -ne "OK\n"
+   else
+	  echo -ne "N/A\n"
+   fi
+done
