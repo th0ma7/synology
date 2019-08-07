8dbbd00035e4e14a7ed3f4d36d0693cc0e7a2bc7
Vincent Fortier <th0ma7@gmail.com>
2019-08-07 19:38:57 -0400

    First revision of tvheadend backup script

---
+++ TVH-backup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+DEST=/volume1/backup/tvheadend
+DATE=`date +%Y%m%d-%H%M`
+
+sudo tar -jcvf tvheadend-backup-$DATE.tar.bz2 -C $DEST \
+   --exclude=.lock \
+   --exclude=tvheadend.pid \
+   /var/packages/tvheadend/target/bin/zap2xml.* \
+   /var/packages/tvheadend/target/bin/tv_grab_file \
+   /var/packages/tvheadend/target/cache \
+   /var/packages/tvheadend/target/.xmltv \
+   /var/packages/tvheadend/target/perl5 \
+   /var/packages/tvheadend/target/.cpan \
+   /var/packages/tvheadend/target/share/tvheadend/data/dvb-scan/atsc/ca-QC-* \
+   /volume1/@appstore/tvheadend/var
